db4b54c716f0e29f20f5855dfccf5983e1fdffa2
Emanuel Ruiz <ruiz.e.642@gmail.com>
2023-12-31 18:08:51 -0300

    se agrego funcion para detectar el string en el parametro del url y mostrar ese string en el saludo

--- hello/urls.py
+++ hello/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"), # "" este string vacio hace referencia a una url sin argumentos, es decir sin nada al final
+    path("<str:name>", views.greet, name="greet"),
     path("brian", views.brian, name="brian")
 ]
