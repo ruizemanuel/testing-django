26251c8c0bf480ce1a2398697b636c5418738650
Emanuel Ruiz <ruiz.e.642@gmail.com>
2023-12-31 17:29:32 -0300

    creacion de urls en hello y se agrego las urls de hello al urls del proyecto general

---
+++ hello/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("", views.index, name="index") # "" este string vacio hace referencia a una url sin argumentos, es decir sin nada al final
+]
