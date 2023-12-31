86909d8f58f67d458659608b73f066d3af6be135
Emanuel Ruiz <ruiz.e.642@gmail.com>
2023-12-31 18:00:55 -0300

    se agrego una nueva ruta y une nueva vista

--- hello/urls.py
+++ hello/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index") # "" este string vacio hace referencia a una url sin argumentos, es decir sin nada al final
+    path("", views.index, name="index"), # "" este string vacio hace referencia a una url sin argumentos, es decir sin nada al final
+    path("brian", views.brian, name="brian")
 ]
