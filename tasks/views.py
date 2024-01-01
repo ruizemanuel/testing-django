c330829f7883748664c7c001a500c5116eb09a4b
Emanuel Ruiz <ruiz.e.642@gmail.com>
2024-01-01 16:47:29 -0300

    se agrego la tabla tasks para guardar ahi las tasks del usuario particular - se agrego logica de empty en el index.html para mostrar algo si tasks esta vacio

--- tasks/views.py
+++ tasks/views.py
@@ -7,11 +7,12 @@ class NewTaskForm(forms.Form):
     task = forms.CharField(label="New Task")
     priority = forms.IntegerField(label="Priority", min_value=1, max_value=5)
 
-tasks = ["foo", "bar", "baz"]
 # Create your views here.
 def index(request):
+    if "tasks" not in request.session:
+        request.session["tasks"] = []
     return render(request, "tasks/index.html", {
-        "tasks": tasks
+        "tasks":  request.session["tasks"]
     })
 
 def add(request):
@@ -19,7 +20,7 @@ def add(request):
         form = NewTaskForm(request.POST) # request.POST contiene la data que el usuario manda en el form
         if form.is_valid():
             task = form.cleaned_data["task"]
-            tasks.append(task)
+            request.session["tasks"] += [task]
             return HttpResponseRedirect(reverse("tasks:index"))
         else:
             return render(request, "tasks/add.html", {
