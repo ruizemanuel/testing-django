459ae15457509cfc5afea138ebca704bda0b2b04
Emanuel Ruiz <ruiz.e.642@gmail.com>
2024-01-01 16:34:28 -0300

    se agrego logica para redireccionar a la lista de tasks despues de agregar una task

--- tasks/views.py
+++ tasks/views.py
@@ -1,5 +1,7 @@
 from django import forms
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 
 class NewTaskForm(forms.Form):
     task = forms.CharField(label="New Task")
@@ -18,6 +20,7 @@ def add(request):
         if form.is_valid():
             task = form.cleaned_data["task"]
             tasks.append(task)
+            return HttpResponseRedirect(reverse("tasks:index"))
         else:
             return render(request, "tasks/add.html", {
                 "form": form # si el formulario no es valido, returna el formulario con sus valores y el error correspondiente
